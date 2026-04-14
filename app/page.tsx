a29838ded4d5ae4dc50083e8cbcc929b4be9840a
touya20 <john.lure29@gmail.com>
2026-04-14 21:25:53 +0800

    Just testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image'
+
+
 
 export default function Home() {
   return (
-    <div>
-        hey
+    <div className="flex items-center justify-center min-h-screen py-2 bg-[#42b8fc] gap-50">
+        <div>
+            <h1 className="text-5xl font-bold text-white mb-4">Welcome to Zenith Care</h1>
+            <p className="text-lg text-white mb-8">Your trusted healthcare partner for a healthier tomorrow.</p>
+        </div>
+        <div>
+            <Image src="/doc1.png" alt="Doctor Image" width={500} height={1000} />
+        </div>
     </div>
   );
 }
--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -11,12 +11,12 @@ export const Navbar = () => {
 
 	return (
 		<>
-			<nav className="flex justify-between items-center h-auto lg:bg-amber-200 bg-blue-200">
-				<div>
+			<nav className="flex justify-between items-center h-15 lg:bg-white-1000">
+				<div className= "font-bold text-2xl m-5">
 					<h1>Zenith Care</h1>
 				</div>
 				<div>
-					<ul className="flex gap-3">
+					<ul className="flex gap-20 text-lg">
 						{Nav.data.map((data, index) => (
 							<li key={index}>
 								 <a href={data.href}>{data.name}</a>
@@ -24,7 +24,7 @@ export const Navbar = () => {
 						))}
 					</ul>
 				</div>
-				<button>
+				<button className="m-5 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
 					Book Appointment
 				</button>
 			</nav>
